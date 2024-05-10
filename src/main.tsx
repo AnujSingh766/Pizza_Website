955a6cf07e839b9bf4725c0e828c98ac13a9f89e
Anuj Singh <anuj.singh00766@gmail.com>
2024-05-10 14:04:47 +0530

    File extension has been chened js to TS of the file store.js located in Redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import {store} from './redux/store.js'
+import {store} from './redux/store.ts'
 import { Provider } from 'react-redux'
 
 const rootElem = document.getElementById('root')
